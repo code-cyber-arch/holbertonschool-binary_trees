3536543ec4f5e03640c0856430ff1721f3026245
amirasabdu <archabdulrm@gmail.com>
2024-04-08 05:59:24 +0000

    is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,9 +9,7 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL || node->right == NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
 	return (1);
 }
