c050b491521b3f025208baf014df0ef1b53af64f
amirasabdu <archabdulrm@gmail.com>
2024-04-08 00:40:30 +0000

    Insert left by checking NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,6 +17,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
 	parent->left = new_node;
 	return (new_node);
