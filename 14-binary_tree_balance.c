b6f0a0e0be2edc4acd3a580a9834b2aff60c2519
amirasabdu <archabdulrm@gmail.com>
2024-04-09 03:20:39 +0000

    Balance updated

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,6 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
