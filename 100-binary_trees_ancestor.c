8ad994b233e0b9f1cb3c17acda74c356202c3e5e
Augusyyy <8851@holbertonstudents.com>
2024-04-10 02:16:39 +0000

    new100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,18 +9,18 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	const binary_tree_t *p_first = first;
-	const binary_tree_t *q_first;
+	binary_tree_t *p_first = first->parent;
+	binary_tree_t *p_second;
 
 	while (p_first)
 	{
-		q_first = second;
-		while (q_first)
+		p_second = second->parent;
+		while (p_second)
 		{
-			if (p_first == q_first)
+			if (p_first == p_second)
 				return (p_first);
 
-			q_first = q_first->parent;
+			p_second = p_second->parent;
 		}
 		p_first = p_first->parent;
 	}
