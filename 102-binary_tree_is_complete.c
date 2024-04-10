cc9b340767005f73daea1266288ba8562c5d072f
Augusyyy <8851@holbertonstudents.com>
2024-04-10 04:12:24 +0000

    new102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height_for_complete - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
  * Return: If tree is NULL, your function must return 0, else return height.
  */
